40d7292df742c81b3ecc8f51f73ac9a06d83444e
Zloy1337 <levtv91@email.com>
2026-05-09 16:15:59 +0100

    update repositories to match async interfaces1

--- GrandmasRecipes.Infrastructure/Repositories/GenericRepository.cs
+++ GrandmasRecipes.Infrastructure/Repositories/GenericRepository.cs
@@ -1,29 +1,25 @@
 ﻿using GrandmasRecipes.Infrastructure.Data;
+using GrandmasRecipes.Infrastructure.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
-public class GenericRepository<T> : IGenericRepository<T> where T : class
+namespace GrandmasRecipes.Infrastructure.Repositories
 {
-    protected readonly ApplicationContext _context;
-    protected readonly DbSet<T> _dbSet;
-
-    public GenericRepository(ApplicationContext context)
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
-        _context = context;
-        _dbSet = _context.Set<T>();
+        protected readonly ApplicationContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IQueryable<T> GetAllAsync() => _dbSet.AsNoTracking();
+        public async Task<T?> GetByIdAsync(Guid id) => await _dbSet.FindAsync(id);
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+        public void UpdateAsync(T entity) => _dbSet.Update(entity);
+        public void DeleteAsync(T entity) => _dbSet.Remove(entity);
+        public async Task SaveAsync() => await _context.SaveChangesAsync();
     }
-
-    public IQueryable<T> GetAll() => _dbSet.AsNoTracking();
-
-    public async Task<T?> GetByIdAsync(Guid id) => await _dbSet.FindAsync(id);
-
-    public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
-
-    public void Update(T entity) => _dbSet.Update(entity);
-
-    public void Delete(T entity) => _dbSet.Remove(entity);
-
-    public async Task SaveAsync() => await _context.SaveChangesAsync();
 }
